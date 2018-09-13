8357d66b740dc303895e85ad06e5166451b964ff
Wolfy-J <wolfy.jd@gmail.com>
2018-09-14 01:12:11 +0300

    configurator

---
+++ src/Loaders/JsonLoader.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+
+namespace Spiral\Configurator\Loaders;
+
+use Spiral\Configurator\LoaderInterface;
+
+class JsonLoader implements LoaderInterface
+{
+
+}
